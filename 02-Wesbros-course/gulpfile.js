297f8061d3964959383950e394345d50e763e6e5
Minato <nadir.berrara@gmail.com>
2017-09-18 13:00:16 +0200

    gulp setup

---
+++ 02-Wesbros-course/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("style", function() {
+  gulp
+    .src("style.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("style.css", ["style"]);
+});
